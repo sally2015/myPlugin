03f71cb17f99e9c5a5a3857001e75d75e6035206
sally2015 <740100506@qq.com>
2016-02-05 12:33:03 +0800

    people

--- myweb/js/script.js
+++ myweb/js/script.js
@@ -1,10 +1,23 @@
 $(function(){
 	
-	var obj={}
+	var oby={}
 	
-	obj.init=function(){
+	oby.event=function(){
+		
+		$('.mynav .language').mouseenter(function(){
+				$('.mynav .language').find('ul').fadeIn()
+		})
+		$('.mynav .language').mouseleave(function(){
+			$('.mynav .language').find('ul').fadeOut();
+		});
+	}
+	oby.init=function(){
+		
+		//第一屏灯的动画
 		$('.light_left').addClass('light_active');
 		$('.light_right').addClass('light_active');
+		
+		oby.event();
 	}
-	obj.init();
+	oby.init();
 })
