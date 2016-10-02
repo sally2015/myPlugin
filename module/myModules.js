262fa5e8e71749dfeaa2c950d6d07518ec45ff2a
sally2015 <740100506@qq.com>
2016-10-02 22:28:04 +0800

    自定义模块化加载器

---
+++ module/myModules.js
@@ -0,0 +1,23 @@
+var myModules = (function Manager(){
+	var modules = {};
+
+	function define(name, deps, impl){
+
+		for (var i = 0; i < deps.length; i++) {
+			
+			deps[i] = modules[deps[i]];
+
+		}
+
+		modules[name] = impl.apply(impl,deps);
+
+	}
+	function get(name){
+		return modules[name];
+	}
+
+	return {
+			get : get,
+			define : define
+		}
+})();
